ce5720a2b087af26a0dfeb8c676e67a8e8b98af5
Cassio-Lima <55493497+Cassio-Lima@users.noreply.github.com>
2020-05-11 18:41:35 +0200

    Add files via upload

---
+++ DownloadSRR.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load  sra_toolkit/x86_64/2.8.2-1
+fastq-dump -I --split-files SRR9273844 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273845 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273846 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273847 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273848 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273849 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273850 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273851 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273852 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273853 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273854 -O /projects/tot3/data_archive
+fastq-dump -I --split-files SRR9273855 -O /projects/tot3/data_archive
